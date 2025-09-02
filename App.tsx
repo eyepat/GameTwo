d48284eb2a27c406e5eabfb0f4129efa669769dd
eyepat <114115559+eyepat@users.noreply.github.com>
2025-09-02 23:27:08 +0200

    Remove old GameList screen and route; keep new 4-group structure

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import PlayerSetupScreen from './src/screens/PlayerSetupScreen';
-import GameListScreen from './src/screens/GameListScreen';
 import CategoryListScreen from './src/screens/CategoryListScreen';
 import GameplayScreen from './src/screens/GameplayScreen';
 import {initI18n} from './src/i18n/i18n';
@@ -17,7 +16,6 @@ export type RootStackParamList = {
   PlayerSetup: undefined;
   MainCategories: undefined;
   GroupGames: {groupId: string};
-  GameList: undefined;
   CategoryList: {gameId: string};
   Gameplay: {gameId: string; categoryId: string; title: string};
 };
@@ -35,7 +33,6 @@ export default function App() {
           <Stack.Screen name="PlayerSetup" component={PlayerSetupScreen} options={{title: ''}} />
           <Stack.Screen name="MainCategories" component={MainCategoriesScreen} options={{title: ''}} />
           <Stack.Screen name="GroupGames" component={GroupGamesScreen} options={{title: ''}} />
-          <Stack.Screen name="GameList" component={GameListScreen} options={{title: ''}} />
           <Stack.Screen name="CategoryList" component={CategoryListScreen} options={{title: ''}} />
           <Stack.Screen name="Gameplay" component={GameplayScreen} options={{title: ''}} />
         </Stack.Navigator>
